d1d697017ec6dc7bd08423a53cb8c831c62b58f5
Abdallah1321 <78796081+Abdallah1321@users.noreply.github.com>
2022-02-24 21:17:07 +0200

    Delete rscapre.r

--- rscapre.r
+++
@@ -1,7 +0,0 @@
-library(rvest)
-library(dplyr)
-
-link = "https://www.imdb.com/search/title/?title_type=tv_episode&num_votes=1000,&sort=user_rating,desc&start=1&ref_=adv_nxt"
-page = read_html(link)
-
-show_name = page %>% html_nodes(".lister-item-header a"[1]) %>% html_text()
